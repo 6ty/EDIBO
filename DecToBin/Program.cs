0d7df265a25fb98d0758dfe1080358d33c0a6388
Arturo Ivo <t27742664@gmail.com>
2020-08-20 19:59:16 +0300

    20200814_16_13

---
+++ DecToBin/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+    public class DecToBin
+    {
+        public static void Main(string[] args)
+        {
+
+            int n, i;
+            int[] a = new int[10];
+
+            Console.Write("Ievadiet skaitli parveidošanai:");
+            
+            n= int.Parse(Console.ReadLine());
+
+                for(i=0;n>0;i++){
+                    a[i]=n%2;
+                    n=n/2;
+                }
+
+            Console.Write("Binarī šis skaitlis būs= ");
+
+                for(i=i-1 ;i>=0;i--){
+                    Console.Write(a[i]);
+                }
+        }
+    }
